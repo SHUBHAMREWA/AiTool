eb893637ba282a928672b05982bbf813c027c645
shubham <shubhamrewamp17@gmail.com>
2025-11-17 19:16:05 +0530

    api added

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
  
- export   const URL=  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDF4gEPW4fiz9WHeuvlWG68ddNGJPetwzU"
+ export  const URL=  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDF4gEPW4fiz9WHeuvlWG68ddNGJPetwzU"
 
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/AiTool/' ,
-  plugins: [react() , tailwindcss(),],
-    // <-- yaha apna repo name dena jaruri hai
-
+  base: '/AiTool/', // ⚠️ Ye jaruri hai — yahi tera repo name hai
+  plugins: [react(), tailwindcss()],
 })
-
